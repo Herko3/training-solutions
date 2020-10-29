a91770bca2bd957a3e7e20015dc51255f75bbbdc
Herko <herko@index.hu>
2020-10-29 16:51:20 +0100

    Elso lecke commit

---
+++ src/main/java/classstructureio/calculator.java
@@ -0,0 +1,17 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+
+        System.out.println("Adjon meg egy egész számot!");
+        Scanner scanner = new Scanner (System.in);
+        int elso = scanner.nextInt();
+        System.out.println("Köszönöm! Most adjon meg még egyet!");
+        int masodik = scanner.nextInt();
+        System.out.println(elso + "+" + masodik);
+        System.out.println(elso + masodik);
+    }
+
+}
