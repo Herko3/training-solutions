731224434460623d08be53bef7a5fb9bdf025af8
Herko <herko@index.hu>
2020-10-30 16:41:24 +0100

    local

---
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,27 @@
+package localvariables;
+
+public class LocalVariablesMain {
+    public static void main(String[] args) {
+        boolean b;
+        // System.out.println(b);
+        b = false;
+        System.out.println(b);
+
+        int a = 2;
+        int i = 3, j = 4;
+        int k = i;
+
+        int c;
+        //System.out.println(c);
+
+        String s = "Hello World";
+        String t = s;
+        System.out.println(t);
+
+        {
+            int x = 0;
+            System.out.println(a);
+        }
+        // System.out.println(x);
+    }
+}
