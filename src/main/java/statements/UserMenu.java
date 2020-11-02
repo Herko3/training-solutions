dc80c8e295a0130c277a09f367b69b63e20fa335
Herko <herko@index.hu>
2020-11-02 23:54:55 +0100

    Bevezetés if, for

---
+++ src/main/java/statements/UserMenu.java
@@ -0,0 +1,24 @@
+package statements;
+
+import java.util.Scanner;
+
+public class UserMenu {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Felhasználók listázása");
+        System.out.println("2. Felhasználó felvétele");
+        System.out.println("Többi: Kilépés");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        if (choice == 1){
+            System.out.println("Felhasználók listázása");
+        }
+        if (choice == 2){
+            System.out.println("Felhasználó felvétele");
+        }
+
+    }
+
+}
