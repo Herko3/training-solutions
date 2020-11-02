0d42b2ab7e2019e1b57472188f808c7000c84384
Herko <herko@index.hu>
2020-11-03 00:04:28 +0100

    Bevezetés if, for

---
+++ src/main/java/statements/Sum.java
@@ -0,0 +1,20 @@
+package statements;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adjon meg 5 számot");
+
+        int sum = 0;
+        for (int i=0;i<5;i++){
+            System.out.println("Add meg a " + (i+1) + ". számot");
+            int number = scanner.nextInt();
+            scanner.nextLine();
+
+            sum = sum + number;
+        }
+        System.out.println("Az összeg: " + sum);
+    }
+}
