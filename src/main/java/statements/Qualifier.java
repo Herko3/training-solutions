82d2664cd4aa99d3957a6c79e1e323f796ff193b
Herko <herko@index.hu>
2020-11-02 23:49:16 +0100

    Bevezetés if, for

---
+++ src/main/java/statements/Qualifier.java
@@ -0,0 +1,18 @@
+package statements;
+
+import java.util.Scanner;
+
+public class Qualifier {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adjon meg egy számot");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+
+        if(number>100){
+            System.out.println("Nagyobb, mint száz");
+        } else {
+            System.out.println("100, vagy kisebb");
+        }
+    }
+}
