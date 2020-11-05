ef16bf4e57c36d34bc6f0e453f376f4f00a070da
Herko <herko@index.hu>
2020-11-05 14:19:13 +0100

    konzi3

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,24 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+
+    int[] nevek = new int[5];
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] nevek = new String[5];
+        for (int i=0; i<nevek.length;i++){
+            System.out.println("Adjon meg egy Nevet");
+            nevek[i]=scanner.nextLine();
+
+        }
+    for (int i=0; i< nevek.length;i++){
+        System.out.println((i+1) +". elem: " + nevek[i]);
+    }
+
+    }
+
+}
