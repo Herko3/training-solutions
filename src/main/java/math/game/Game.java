c1603ad7de63799487d75b572d6746019928cbcc
Herko <herko@index.hu>
2020-11-05 01:35:56 +0100

    Math & Random

---
+++ src/main/java/math/game/Game.java
@@ -0,0 +1,11 @@
+package math.game;
+
+public class Game {
+
+    public static void main(String[] args) {
+        Warrior warrior1 = new Warrior("Jack",new Point(5,2));
+        Warrior warrior2 = new Warrior("Joe", new Point(3,6));
+
+
+    }
+}
