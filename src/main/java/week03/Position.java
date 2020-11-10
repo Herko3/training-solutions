f6e11e540c70121891c8a6dec33573dc6f8beaac
Herko <herko@index.hu>
2020-11-10 13:36:38 +0100

    konzi week03/1

---
+++ src/main/java/week03/Position.java
@@ -0,0 +1,47 @@
+package week03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Position {
+
+    private String name;
+    private int bonus;
+
+    public Position(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + bonus;
+    }
+
+    public static void main(String[] args) {
+        Position boss = new Position("Boss",250000);
+        Position manager = new Position("Manager",200000);
+        Position employee = new Position("Employee",120000);
+
+        List<Position> position = new ArrayList<>();
+        position.add(boss);
+        position.add(manager);
+        position.add(employee);
+
+        for (Position test : position){
+            if (test.getBonus()>150000){
+                System.out.println(test);
+            }
+        }
+
+
+    }
+}
