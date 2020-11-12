0d2ecbf56379a90ec867bf1d3d489933d357fc8b
Herko <herko@index.hu>
2020-11-12 13:58:30 +0100

    konzi week3

---
+++ src/main/java/week03/Phone.java
@@ -0,0 +1,33 @@
+package week03;
+
+public class Phone {
+
+    private int prefix;
+    private int number;
+
+    public Phone(int prefix, int number) {
+        this.prefix = prefix;
+        this.number = number;
+    }
+
+    public int getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(int prefix) {
+        this.prefix = prefix;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "A telefonszám prefixe:" + prefix + "\n A telefonszám szám része: " + number;
+    }
+}
---
+++ src/main/java/week03/PhoneParser.java
@@ -0,0 +1,17 @@
+package week03;
+
+public class PhoneParser {
+
+    public Phone parsePhone(String number){
+        String[] part = number.split("-");
+        int prefix=Integer.parseInt(part[0]);
+        int num = Integer.parseInt(part[1]);
+        return new Phone(prefix,num);
+    }
+
+    public static void main(String[] args) {
+        PhoneParser test = new PhoneParser();
+        System.out.println(test.parsePhone("30-1234567"));
+
+    }
+}
