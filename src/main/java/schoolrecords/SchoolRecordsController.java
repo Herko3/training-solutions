ef580bcf90db9595d9e8c3f75c878196a5518c7f
Herko <herko@index.hu>
2020-11-17 15:43:55 +0100

    Projekt

--- src/main/java/schoolrecords/SchoolRecordsController.java
+++ src/main/java/schoolrecords/SchoolRecordsController.java
@@ -17,62 +17,74 @@ public class SchoolRecordsController {
                 new Subject("fizika"),
                 new Subject("kémia"));
         tutor = new Tutor("Nagy Csilla", subjects);
-        classRecords = new ClassRecords("Fourth Grade A", new Random(5));
+        classRecords = new ClassRecords("Fourth Grade A", new Random());
     }
 
     public void printMenu() {
         System.out.println(
                 "1. Diákok nevének listázása\n" +
-                "2. Diák név alapján keresése\n" +
-                "3. Diák létrehozása\n" +
-                "4. Diák név alapján törlése\n" +
-                "5. Diák feleltetése\n" +
-                "6. Osztályátlag kiszámolása\n" +
-                "7. Tantárgyi átlag kiszámolása\n" +
-                "8. Diákok átlagának megjelenítése\n" +
-                "9. Diák átlagának kiírása\n" +
-                "10. Diák tantárgyhoz tartozó átlagának kiírása\n" +
-                "11. Kilépés");
+                        "2. Diák név alapján keresése\n" +
+                        "3. Diák létrehozása\n" +
+                        "4. Diák név alapján törlése\n" +
+                        "5. Diák feleltetése\n" +
+                        "6. Osztályátlag kiszámolása\n" +
+                        "7. Tantárgyi átlag kiszámolása\n" +
+                        "8. Diákok átlagának megjelenítése\n" +
+                        "9. Diák átlagának kiírása\n" +
+                        "10. Diák tantárgyhoz tartozó átlagának kiírása\n" +
+                        "11. Kilépés");
         runMenu();
     }
 
-    public void runMenu(){
+    public void runMenu() {
         Scanner scanner = new Scanner(System.in);
-        try{
-            int choice = scanner.nextInt();
-            while(choice !=11){
-                switch (choice){
-                    case 1: classRecords.listStudentNames();
-                    cont();
-                    case 2:
-                        System.out.println("Adja meg a keresendő nevet:");
-                        String name = scanner.nextLine();
-                        classRecords.findStudentByName(name);
-                        cont();
-                    case 3:
-                        System.out.println("Adja meg az új diák nevét:");
-                        scanner.skip("\n");
-                        String addName =  scanner.nextLine();
-                        classRecords.addStudent(new Student(addName));
-                        cont();
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                    case 10:
-                    default:
-                        printMenu();
-                }
-            }
-        } catch (InputMismatchException ex){
-            printMenu();
+        int choice = 0;
+        try {
+
+            choice = scanner.nextInt();
+
+        } catch (InputMismatchException ex) {
+            System.out.println("Csak számokat írjon be");
+            cont();
+        }
+
+        switch (choice) {
+            case 1:
+                System.out.println(classRecords.listStudentNames());
+                cont();
+            case 2:
+                System.out.println("Adja meg a keresendő nevet:");
+                scanner.skip("\n");
+                String name = scanner.nextLine();
+                System.out.println(classRecords.findStudentByName(name));
+                cont();
+            case 3:
+                System.out.println("Adja meg az új diák nevét:");
+                scanner.skip("\n");
+                String addName = scanner.nextLine();
+                classRecords.addStudent(new Student(addName));
+                cont();
+            case 4:
+                System.out.println("Törölni kívánt diák neve:");
+                scanner.skip("\n");
+                String delete = scanner.nextLine();
+                classRecords.removeStudent(classRecords.findStudentByName(delete));
+                cont();
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                System.exit(0);
+            default:
+                printMenu();
         }
 
     }
 
-    public void cont(){
+    public void cont() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Folytatáshoz nyomja meg az ENTER-t");
         scanner.nextLine();
@@ -81,9 +93,9 @@ public class SchoolRecordsController {
 
 
     public static void main(String[] args) {
-    SchoolRecordsController controller = new SchoolRecordsController();
-    controller.initSchool();
-    controller.printMenu();
+        SchoolRecordsController controller = new SchoolRecordsController();
+        controller.initSchool();
+        controller.printMenu();
     }
 
 }
