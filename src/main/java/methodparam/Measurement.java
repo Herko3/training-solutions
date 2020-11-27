489ae5992f5c9362ea5d8367389883d04d1e2ee4
Herko <herko@index.hu>
2020-11-27 21:15:01 +0100

    paraméterek és visszatérés

---
+++ src/main/java/methodparam/Measurement.java
@@ -0,0 +1,49 @@
+package methodparam;
+
+public class Measurement {
+
+    private double[] measures;
+
+    public Measurement(double[] measures) {
+        this.measures = measures;
+    }
+
+    public int findFirstIndexInLimit(int lower, int upper) {
+        int index = 0;
+        for (double i : measures) {
+            if (i > lower && i < upper) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    public double minimum() {
+        double minimum = Integer.MAX_VALUE;
+        for (double i : measures) {
+            if (minimum > i) {
+                minimum = i;
+            }
+        }
+        return minimum;
+    }
+
+    public double maximum() {
+        double maximum = Integer.MIN_VALUE;
+        for (double i : measures) {
+            if (maximum < i) {
+                maximum = i;
+            }
+        }
+        return maximum;
+    }
+
+    public ExtremValues minmax() {
+        ExtremValues minmax = new ExtremValues(minimum(),maximum());
+
+        return minmax;
+    }
+
+
+}
