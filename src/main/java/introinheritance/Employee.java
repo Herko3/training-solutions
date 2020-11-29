81427415cb63918b60402b8accdff3011f531cfe
Herko <herko@index.hu>
2020-11-29 22:37:17 +0100

    inheritance

---
+++ src/main/java/introinheritance/Employee.java
@@ -0,0 +1,19 @@
+package introinheritance;
+
+public class Employee extends Person {
+
+    private double salary;
+
+    public Employee(String name, String address, double salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+
+    public void raiseSalary(int percent){
+        salary += salary * percent/100;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
