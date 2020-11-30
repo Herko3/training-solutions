e4533cdc85007b013c76440ce3ff3fe79f8e3979
Herko <herko@index.hu>
2020-11-30 18:00:36 +0100

    week06

---
+++ src/test/java/week06d01/SeparatedSumTest.java
@@ -0,0 +1,18 @@
+package week06d01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeparatedSumTest {
+
+    @Test
+    void testSum(){
+        SeparatedSum separatedSum = new SeparatedSum();
+        SumResults result = separatedSum.sum("56,67;6,8;3;-12;-16,7");
+
+        assertEquals(66.47,result.getPositive(),0.005);
+        assertEquals(-28.7,result.getNegative(),0.005);
+    }
+
+}
