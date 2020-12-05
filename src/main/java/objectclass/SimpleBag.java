f14931fb93c4c547b6a501f7aba72a7b08f38004
Herko <herko@index.hu>
2020-12-05 10:48:04 +0100

    object ősosztály

---
+++ src/main/java/objectclass/SimpleBag.java
@@ -0,0 +1,50 @@
+package objectclass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleBag {
+
+    private List<Object> items = new ArrayList<>();
+
+    private int cursor = -1;
+
+    public void putItem(Object item) {
+        items.add(item);
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void beforeFirst() {
+        cursor = -1;
+    }
+
+    public boolean hasNext() {
+        return size() > (cursor + 1);
+    }
+
+    public Object next(){
+        cursor++;
+        return items.get(cursor);
+    }
+
+
+    public boolean contains (Object item){
+        for(Object obj : items){
+            if(obj.equals(item)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCursor() {
+        return cursor;
+    }
+}
