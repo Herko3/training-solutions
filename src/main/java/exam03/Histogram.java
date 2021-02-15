3b45610f451139f1b8008c1c59e01994af781899
Herko <herko@index.hu>
2021-02-15 17:15:12 +0100

    exam03 - histogram done

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,6 +1,35 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Histogram {
 
+    public String createHistogram(BufferedReader reader) {
+        StringBuilder sb = new StringBuilder();
+        try (reader) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(createLine(line));
+            }
 
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file", ioe);
+        }
+        return sb.toString();
+    }
+
+    private String createLine(String s) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            int number = Integer.parseInt(s);
+            for (int i = 0; i < number; i++) {
+                sb.append("*");
+            }
+
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("Not a number: " + s);
+        }
+        return sb.append("\n").toString();
+    }
 }
