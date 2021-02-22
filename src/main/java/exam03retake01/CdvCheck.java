f9ba956a06e4d783f2c60489da98da702e860df7
Herko <herko@index.hu>
2021-02-22 19:26:34 +0100

    exam03retake CDV done

--- src/main/java/exam03retake01/CdvCheck.java
+++ src/main/java/exam03retake01/CdvCheck.java
@@ -2,5 +2,20 @@ package exam03retake01;
 
 public class CdvCheck {
 
-
+    public boolean check(String number) {
+        if (number.length() != 10) {
+            throw new IllegalArgumentException("Not valid input");
+        }
+        int sum = 0;
+        int lastDigit;
+        try {
+            lastDigit = Integer.parseInt(number.substring(9));
+            for (int i = 0; i < 9; i++) {
+                sum += (i+1) * Integer.parseInt(number.substring(i, i + 1));
+            }
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("Not valid input");
+        }
+        return (sum % 11) == lastDigit;
+    }
 }
