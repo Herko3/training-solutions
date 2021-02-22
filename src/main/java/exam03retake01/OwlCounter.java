c1c98968fc1252c672c8a0c69d44cc222d93c497
Herko <herko@index.hu>
2021-02-22 18:52:39 +0100

    exam03retake OwlCounter done

--- src/main/java/exam03retake01/OwlCounter.java
+++ src/main/java/exam03retake01/OwlCounter.java
@@ -1,6 +1,23 @@
 package exam03retake01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class OwlCounter {
 
+    Map<String,Integer> owlsPerState = new HashMap<>();
 
+    public void readFromFile(BufferedReader reader) throws IOException {
+        String line;
+        while((line= reader.readLine()) != null){
+            String[] parts = line.split("=");
+            owlsPerState.put(parts[0],Integer.parseInt(parts[1]));
+        }
+    }
+
+    public int getNumberOfOwls(String state){
+        return owlsPerState.get(state);
+    }
 }
