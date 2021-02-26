4d9dea284d3345e90671933e0679e36c356f26ca
Herko <herko@index.hu>
2021-02-26 17:42:23 +0100

    exam03retake02 TodoList done

--- src/main/java/exam03retake02/Todo.java
+++ src/main/java/exam03retake02/Todo.java
@@ -1,6 +1,38 @@
 package exam03retake02;
 
-public class Todo {
+public class Todo implements Comparable<Todo> {
 
+    private String text;
+    private int priority;
+    private State state;
 
+    public Todo(String name, int priority) {
+        if (priority < 1 || priority > 5) {
+            throw new IllegalArgumentException("Priority must be between 1-5");
+        }
+        this.text = name;
+        this.priority = priority;
+        state = State.NON_COMPLETED;
+    }
+
+    public void complete() {
+        state = State.COMPLETED;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    @Override
+    public int compareTo(Todo o) {
+        return priority - o.priority;
+    }
 }
--- src/main/java/exam03retake02/TodoList.java
+++ src/main/java/exam03retake02/TodoList.java
@@ -1,6 +1,40 @@
 package exam03retake02;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TodoList {
 
+    private List<Todo> todos = new ArrayList<>();
 
+    public void addTodo(Todo todo) {
+        todos.add(todo);
+    }
+
+    public List<Todo> getTodos() {
+        return new ArrayList<>(todos);
+    }
+
+    public long getNumberOfItemsLeft() {
+        return todos.stream()
+                .filter(todo -> todo.getState() == State.NON_COMPLETED)
+                .count();
+    }
+
+    public List<String> getMostImportantTodosText() {
+        List<String> result = new ArrayList<>();
+        Collections.sort(todos);
+        int lowestPriority = todos.get(0).getPriority();
+        for (Todo todo : todos) {
+            if (todo.getPriority() == lowestPriority) {
+                result.add(todo.getText());
+            }
+        }
+        return result;
+    }
+
+    public void deleteCompleted() {
+        todos.removeIf(todo -> todo.getState() == State.COMPLETED);
+    }
 }
